89d2647f64ea7a4fb1c2ec7f42e6a22db80955f7
Williams Chidi <williamschidi1@gmail.com>
2018-07-27 09:01:10 +0100

    Removed simulator import

--- src/components/Lab.js
+++ src/components/Lab.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Simulator from './Simulator/Simulator';
+// import Simulator from './Simulator/Simulator';
 import Header from './Header/Header';
 
 const styles = (theme) => ({

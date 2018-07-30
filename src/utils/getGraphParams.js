999a5a6e101603b88c2031367c3a28d3c53df719
Williams Chidi <williamschidi1@gmail.com>
2018-07-30 01:15:04 +0100

    Combined fres and xaxis in single graphing fn

--- src/utils/getGraphParams.js
+++ src/utils/getGraphParams.js
@@ -1,17 +1,28 @@
-import getXAxis from './graphing/getXAxis';
-import getFRes from './graphing/getFRes';
+import getXAxis from './getXAxis';
+import getFRes from './getFRes';
+import * as lab from 'datacomm-lab';
 
 export default (arr, name) => {
-  const fres = getFRes(arr);
+  // Get array's x-axis
+  const arrx = Array.apply(null, Array(arr.length)).map((x, i, a) =>
+    parseFloat((i / a.length).toFixed(4))
+  );
+
+  // Get frequency response
+  const fx = Array.apply(null, Array(arr.length / 2 + 1)).map((x, i) => +i);
+  const sig = new lab.Signal(arr.length);
+  sig.signal = arr;
+  const fy = sig.getFrequencyResponse();
+
   return {
     t: {
-      x: getXAxis(arr.length),
+      x: arrx,
       y: arr,
       tit: name + ' signal time response',
     },
     f: {
-      x: fres.fx,
-      y: fres.fy,
+      x: fx,
+      y: fy,
       tit: name + ' signal frequency response',
       xmas: 128,
     },

80a799fd1d8e832b590ff459198712a477fc22d0
Williams Chidi <williamschidi1@gmail.com>
2018-08-04 03:11:22 +0100

    Using lab group for spectrometer

--- src/containers/SignalGenerator.js
+++ src/containers/SignalGenerator.js
@@ -1,21 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
 import defaults from '../config/defaults';
 import ButtonSelect from './ButtonSelect';
 import ButtonCrement from './ButtonCrement';
-import './SignalGenerator.css';
-
-const styles = (theme) => ({
-  formControl: {
-    flex: 1,
-    margin: theme.spacing.unit,
-  },
-  formSpace: {
-    display: 'flex',
-    margin: [[theme.spacing.unit, -theme.spacing.unit]],
-  },
-});
+import LabGroup from '../components/LabGroup';
 
 class SignalGenerator extends Component {
   state = {
@@ -31,52 +19,66 @@ class SignalGenerator extends Component {
   render() {
     return (
       <div>
-        <div className="sgInput">
-          <div className="selectLabel">Waveshape</div>
-          <ButtonSelect
-            onChange={(val) =>
-              this.handleSelectChange(
-                'waveshape',
-                val,
-                this.props.handleTypeChange
-              )
-            }
-          />
-        </div>
-
-        <div className="flexparent">
-          <div className="sgInput">
-            <div className="selectLabel">Signal frequency</div>
-            <ButtonCrement
-              options={defaults.allFa}
-              startIndex={defaults.allFa.indexOf(defaults.Fa)}
-              handleChange={(val) =>
-                this.handleSelectChange('fa', val, this.props.handleFaChange)
-              }
-            />
-          </div>
-          <div className="sgInput">
-            <div className="selectLabel">Sampling frequency</div>
-            <ButtonCrement
-              options={defaults.allFs}
-              startIndex={defaults.allFs.indexOf(defaults.Fs)}
-              handleChange={(val) =>
-                this.handleSelectChange('fs', val, this.props.handleFsChange)
-              }
-            />
-          </div>
-        </div>
+        <LabGroup
+          title="Spectrometer"
+          inputs={[
+            {
+              label: 'Waveshape',
+              component: (
+                <ButtonSelect
+                  onChange={(val) =>
+                    this.handleSelectChange(
+                      'waveshape',
+                      val,
+                      this.props.handleTypeChange
+                    )
+                  }
+                />
+              ),
+            },
+            {
+              label: 'Signal frequency',
+              component: (
+                <ButtonCrement
+                  options={defaults.allFa}
+                  startIndex={defaults.allFa.indexOf(defaults.Fa)}
+                  handleChange={(val) =>
+                    this.handleSelectChange(
+                      'fa',
+                      val,
+                      this.props.handleFaChange
+                    )
+                  }
+                />
+              ),
+            },
+            {
+              label: 'Sampling frequency',
+              component: (
+                <ButtonCrement
+                  options={defaults.allFs}
+                  startIndex={defaults.allFs.indexOf(defaults.Fs)}
+                  handleChange={(val) =>
+                    this.handleSelectChange(
+                      'fs',
+                      val,
+                      this.props.handleFsChange
+                    )
+                  }
+                />
+              ),
+            },
+          ]}
+        />
       </div>
     );
   }
 }
 
 SignalGenerator.propTypes = {
-  classes: PropTypes.object.isRequired,
-  theme: PropTypes.object.isRequired,
   handleTypeChange: PropTypes.func.isRequired,
   handleFaChange: PropTypes.func.isRequired,
   handleFsChange: PropTypes.func.isRequired,
 };
 
-export default withStyles(styles, { withTheme: true })(SignalGenerator);
+export default SignalGenerator;

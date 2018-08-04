25839d8cc7ade6ce79a154f85541e5fa0820c990
Williams Chidi <williamschidi1@gmail.com>
2018-08-04 14:46:00 +0100

    fixed floating app title

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,8 +6,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <a href="https://github.com/chidiwilliams">
-        <img  style={{position: 'absolute', top: 0, left: 0, border: 0}} src="https://s3.amazonaws.com/github/ribbons/forkme_left_red_aa0000.png" alt="Fork me on GitHub"></img>
+        <a href="https://github.com/chidiwilliams/datacomm-client">
+          <img
+            style={{ position: 'absolute', top: 0, left: 0, border: 0 }}
+            src="https://s3.amazonaws.com/github/ribbons/forkme_left_red_aa0000.png"
+            alt="Fork me on GitHub"
+          />
         </a>
         <Lab />
       </div>
